7f1bcaedb16e4a2bfc77dad68c6536f63786afcd
chtiyala sri ram <sriramchityala7522@gmail.com>
2025-12-30 22:47:20 +0530

    added first day code

---
+++ js.js
@@ -0,0 +1,91 @@
+const fliters={
+    brightness:{
+        value:100,
+        min:0,
+        max:200,
+        unit:"%"
+    },
+    contrast:{
+        value:100,
+        min:0,
+        max:200,
+        unit:"%"
+    },
+    exposure:{
+        value:100,
+        min:0,
+        max:200,
+        unit:"%"
+    },
+    saturation:{
+        value:100,
+        min:0,
+        max:200,
+        unit:"%"
+    },
+    hueRotation:{
+        value:0,
+        min:0,
+        max:360,
+        unit:"deg"
+    },
+    blur:{
+        value:0,
+        min:0,
+        max:20,
+        unit:"px"
+    },
+    grayscale:{
+        value:0,
+        min:0,
+        max:100,
+        unit:"%"
+    },
+    sepia:{
+        value:0,
+        min:0,
+        max:100,
+        unit:"%"
+    },
+    opacity:{
+        value:100,
+        min:0,
+        max:100,
+        unit:"%"
+    },
+    invert:{
+        value:0,
+        min:0,
+        max:100,
+        unit:"%"
+    }
+} 
+
+const fliterbox=document.querySelector(".fliters");
+ 
+
+function createFliterElement(name,unit="%",value,min,max){
+    const div=document.createElement("div")
+    div.classList.add("fliter")
+
+    const input=document.createElement("input")
+    input.type="range"
+    input.min=min
+    input.max=max
+    input.value=value
+    input.id=name
+
+    const p=document.createElement("p")
+    p.innerText=name
+    
+    div.appendChild(p)
+    div.appendChild(input)
+
+    return div
+}
+
+Object.keys(fliters).forEach(key =>{
+         const fliterelement=createFliterElement(key,fliters[key].unit,fliters[key].value,fliters[key].min,fliters[key].max)
+         fliterbox.appendChild(fliterelement)
+})
+
